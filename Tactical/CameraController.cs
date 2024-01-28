3d8a3054c6042745a009e7be24a8f02fd921b07c
ArtixBot <williamwongcastle@gmail.com>
2024-01-28 10:24:10 -0500

    Adds vignette effect when selecting an ability.

--- Tactical/CameraController.cs
+++ Tactical/CameraController.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 public partial class CameraController : Camera2D
 {
+
+	public ColorRect cameraEffect;
+	public ShaderMaterial vignetteEffect;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		cameraEffect = GetNode<ColorRect>("ColorRect");
+		vignetteEffect = GD.Load<ShaderMaterial>("res://Tactical/UI/Shaders/BlurVignette.tres");
 		this.Position = new Vector2(960, 540);
 	}
 
@@ -27,14 +32,23 @@ public partial class CameraController : Camera2D
 		Vector2 oldZoom = this.Zoom;
 		Vector2 newZoom = this.Zoom + new Vector2(zoomAmount, zoomAmount);
 
+		// Use vectors so we can use Godot's in-built Lerp function.
+		Vector2 startBlurRadius = new Vector2(0.01f, 0.0f);
+		Vector2 endBlurRadius = new Vector2(0.2f, 0.0f);
+		cameraEffect.Material = vignetteEffect;
+		vignetteEffect.SetShaderParameter("blur_radius", startBlurRadius);
+
 		float currentTime = 0f;
 		while (currentTime <= duration){
             float normalized = Math.Min((float)(currentTime / duration), 1.0f);
 			this.Zoom = oldZoom.Lerp(newZoom, EaseOut(normalized));
 
+			vignetteEffect.SetShaderParameter("blur_radius", startBlurRadius.Lerp(endBlurRadius, EaseOut(normalized)).X);
+
 			await Task.Delay(1);
             currentTime += (float)GetProcessDeltaTime();		// Not using PhysicsProcess since this is graphical effect only.
         }
+
 		isCinematic = true;
 		return true;
 	}
@@ -43,14 +57,22 @@ public partial class CameraController : Camera2D
 		Vector2 oldZoom = this.Zoom;
 		Vector2 newZoom = new Vector2(1.0f, 1.0f);
 
+		// Use vectors so we can use Godot's in-built Lerp function.
+		Vector2 startBlurRadius = new Vector2(0.2f, 0.0f);
+		Vector2 endBlurRadius = new Vector2(0.01f, 0.0f);
+		vignetteEffect.SetShaderParameter("blur_radius", startBlurRadius);
+
 		float currentTime = 0f;
 		while (currentTime <= duration){
             float normalized = Math.Min((float)(currentTime / duration), 1.0f);
 			this.Zoom = oldZoom.Lerp(newZoom, EaseOut(normalized));
 
+			vignetteEffect.SetShaderParameter("blur_radius", startBlurRadius.Lerp(endBlurRadius, EaseOut(normalized)).X);
+
 			await Task.Delay(1);
             currentTime += (float)GetProcessDeltaTime();		// Not using PhysicsProcess since this is graphical effect only.
         }
+		cameraEffect.Material = null;
 		isCinematic = false;
 		return true;
 	}
