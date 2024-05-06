73f5e18023a003beb7e4ae214f4d8167fccf2e8c
ArtixBot <williamwongcastle@gmail.com>
2024-05-05 21:31:08 -0400

    Add custom tag handling to ability detail panels by using {}. e.g. {Cond|on activate} will apply the appropriate formatting to on activate.

--- Tactical/UI/Abilities/AbilityDetailPanel.cs
+++ Tactical/UI/Abilities/AbilityDetailPanel.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Godot;
 
@@ -36,14 +38,38 @@ public partial class AbilityDetailPanel : Control
 		abilityName.Text = _ability.NAME;
 		string rangeText = (_ability.TYPE == AbilityType.REACTION) ? "" : $"\t\t[img=24]res://Sprites/range.png[/img] {_ability.MIN_RANGE} - {_ability.MAX_RANGE}";
 		abilityInfo.Text = $"[font n='res://Assets/Jost-Medium.ttf' s=16]{_ability.TYPE}"  + $"\t\t[img=24]res://Sprites/cooldown.png[/img] {_ability.BASE_CD}" + rangeText;
-		AbilityDesc = "[font n='res://Assets/Inter-Regular.ttf' s=16]" + _ability.STRINGS.GetValueOrDefault("GENERIC", "") + "[/font]";
+		AbilityDesc = ParseCustomTags(_ability.STRINGS.GetValueOrDefault("GENERIC", ""));
 
 		for (int i = 0; i < _ability.BASE_DICE.Count; i++){
             AbilityDie node = (AbilityDie) abilityDie.Instantiate();
 			nodeToAddDiceTo.AddChild(node);
 
 			node.Die = _ability.BASE_DICE[i];
-			node.DieDesc =  _ability.STRINGS.GetValueOrDefault(node.Die.DieId, "");
+			node.DieDesc =  ParseCustomTags(_ability.STRINGS.GetValueOrDefault(node.Die.DieId, ""));
 		}
+	}
+
+	// TODO: This will not work well for internationalized strings.
+	private string ParseCustomTags(string s){
+        MatchCollection matches = new Regex(@"(?<=\{)(.*?)(?=\})").Matches(s);
+		for (int i = 0; i < matches.Count; i++){
+            Match match = matches[i];
+			// e.g.: Convert {Slash|9-13} to [color][b]Slash 9-13[/b][/color].
+            if (match.Value.Contains("Slash") || match.Value.Contains("Pierce") || match.Value.Contains("Blunt") || match.Value.Contains("Eldritch")){
+				string[] attackTypeAndRange = match.Value.Split("|");
+				string attackType = attackTypeAndRange.First();
+				string damageRange = attackTypeAndRange.Last();
+
+				string replacementString = "[color=#FF4E50][b]" + attackType + " " + damageRange + "[/b][/color]";
+                s = s.Replace("{" + match.Value + "}", replacementString);
+            }
+			// e.g.: Convert {Cond|On activate} to [color]On activate[/color].
+			else if (match.Value.Contains("Cond")){
+				string textToHighlight = match.Value.Split("|").Last();
+				string replacementString = "[color=#4cf]" + textToHighlight + "[/color]";
+                s = s.Replace("{" + match.Value + "}", replacementString);
+			}
+        }
+		return s;
 	}
 }
